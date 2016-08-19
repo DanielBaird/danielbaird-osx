8abf93e4c836c9a0479acb94498d257008dff12d
Daniel Baird <daniel@danielbaird.com>
2016-08-19 12:45:15 +1000

    checks for existing profile

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,1 +1,12 @@
-ln -s ~/.config/danielbaird-osx/bin/bash-profile.sh ~/.bash_profile
+
+# check for existing ~/.bash-profile
+if [ -f ~/.bash_profile ]; then
+    echo "You already have a .bash_profile!"
+    read -p "Remove existing .bash_profile (y/n)? " -n 1 -r
+    echo    # move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        rm ~/.bash_profile && echo "Removed"
+    fi
+fi
+ln -s ~/.config/danielbaird-osx/bin/bash-profile.sh ~/.bash_profile && echo "Successfully linked new .bash_profile"
