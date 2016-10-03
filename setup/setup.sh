1c247b14a9cccc401be166e2730500d0e895c4f2
Daniel Baird <daniel@danielbaird.com>
2016-10-04 09:54:31 +1000

    added ssh config

--- setup/setup.sh
+++ setup/setup.sh
@@ -10,3 +10,15 @@ if [ -f ~/.bash_profile ]; then
     fi
 fi
 ln -s ~/.config/danielbaird-osx/bin/bash-profile.sh ~/.bash_profile && echo "Successfully linked new .bash_profile"
+
+# check for existing ~/.ssh/config
+if [ -f ~/.ssh/config ]; then
+    echo "You already have a .ssh/config !"
+    read -p "Remove existing .ssh/config (y/n)? " -n 1 -r
+    echo    # move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        rm ~/.ssh/config && echo "Removed"
+    fi
+fi
+ln -s ~/.config/danielbaird-osx/ssh/config ~/.ssh/config && echo "Successfully linked new .ssh/config"
