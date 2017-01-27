af54ad3cd82a5b1dd31506c6d42aba5f80613885
Daniel Baird <daniel@danielbaird.com>
2017-01-27 13:44:53 +1000

    additional config; git

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,24 +1,38 @@
+source ~/.config/danielbaird-osx/bin/bash-color-vars.sh
+CHECK="${GREEN}✓${RESTORE}"
 
 # check for existing ~/.bash-profile
-if [ -f ~/.bash_profile ]; then
-    echo "You already have a .bash_profile!"
+if [ -e ~/.bash_profile ]; then
     read -p "Remove existing .bash_profile (y/n)? " -n 1 -r
     echo    # move to a new line
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
-        rm ~/.bash_profile && echo "Removed"
+        rm ~/.bash_profile
     fi
 fi
-ln -s ~/.config/danielbaird-osx/bin/bash-profile.sh ~/.bash_profile && echo "Successfully linked new .bash_profile"
+ln -s ~/.config/danielbaird-osx/bin/bash-profile.sh ~/.bash_profile && echo "$CHECK linked new .bash_profile"
 
 # check for existing ~/.ssh/config
-if [ -f ~/.ssh/config ]; then
-    echo "You already have a .ssh/config !"
+if [ -e ~/.ssh/config ]; then
     read -p "Remove existing .ssh/config (y/n)? " -n 1 -r
     echo    # move to a new line
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
-        rm ~/.ssh/config && echo "Removed"
+        rm ~/.ssh/config
     fi
 fi
-ln -s ~/.config/danielbaird-osx/ssh/config ~/.ssh/config && echo "Successfully linked new .ssh/config"
+ln -s ~/.config/danielbaird-osx/ssh/config ~/.ssh/config && echo "$CHECK linked new .ssh/config"
+
+pushd ~/.config/danielbaird-osx/config-dir > /dev/null
+for CFGDIR in *; do
+    if [ -e "$HOME/.config/$CFGDIR" ]; then
+        read -p "Remove existing .config/$CFGDIR (y/n)? " -n 1 -r
+        echo    # move to a new line
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            rm "$HOME/.config/$CFGDIR"
+        fi
+    fi
+    ln -s "$HOME/.config/danielbaird-osx/config-dir/$CFGDIR" "$HOME/.config/$CFGDIR" && echo "$CHECK linked new $CFGDIR config"
+done
+popd > /dev/null
