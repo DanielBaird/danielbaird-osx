dd8d383d67925f312dc2fae4c2d2f5000c81ba6a
Daniel Baird <daniel@danielbaird.com>
2017-01-31 17:25:01 +1000

    added Homebrew

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,51 +1,68 @@
 source ~/.config/danielbaird-osx/bin/bash-color-vars.sh
-CHECK="${GREEN}✓${RESTORE}"
+
+function declare {
+  echo "${BLUE}◈◈◈setup◈◈◈  ${YELLOW}$1${RESTORE}"
+}
+function demand {
+  read -p "${BLUE}◈◈◈setup◈◈◈  ${YELLOW}► $1${RESTORE}" -n 1 -r
+}
+function reassure {
+  echo "${BLUE}◈◈◈setup◈◈◈  ${YELLOW}${GREEN}✓${YELLOW} $1${RESTORE}"
+  echo
+}
 
 # link in ~/.bash-profile -----------------------------------------------------
 if [ -e ~/.bash_profile ]; then
-    read -p "Remove existing .bash_profile (y/n)? " -n 1 -r
+    demand "Remove existing .bash_profile (y/n)? "
     echo    # move to a new line
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
         rm ~/.bash_profile
     fi
 fi
-ln -s ~/.config/danielbaird-osx/bin/bash-profile.sh ~/.bash_profile && echo "$CHECK link new .bash_profile"
+ln -s ~/.config/danielbaird-osx/bin/bash-profile.sh ~/.bash_profile && reassure "link new .bash_profile"
 
 # link in ~/.ssh/config -------------------------------------------------------
 if [ -e ~/.ssh/config ]; then
-    read -p "Remove existing .ssh/config (y/n)? " -n 1 -r
+    demand "Remove existing .ssh/config (y/n)? "
     echo    # move to a new line
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
         rm ~/.ssh/config
     fi
 fi
-ln -s ~/.config/danielbaird-osx/ssh/config ~/.ssh/config && echo "$CHECK link new .ssh/config"
+ln -s ~/.config/danielbaird-osx/ssh/config ~/.ssh/config && reassure "link new .ssh/config"
 
 # link in all the .config directories -----------------------------------------
 pushd ~/.config/danielbaird-osx/config-dir > /dev/null
 for CFGDIR in *; do
     if [ -e "$HOME/.config/$CFGDIR" ]; then
-        read -p "Remove existing .config/$CFGDIR (y/n)? " -n 1 -r
+        demand "Remove existing .config/$CFGDIR (y/n)? "
         echo    # move to a new line
         if [[ $REPLY =~ ^[Yy]$ ]]
         then
             rm "$HOME/.config/$CFGDIR"
         fi
     fi
-    ln -s "$HOME/.config/danielbaird-osx/config-dir/$CFGDIR" "$HOME/.config/$CFGDIR" && echo "$CHECK link new $CFGDIR config"
+    ln -s "$HOME/.config/danielbaird-osx/config-dir/$CFGDIR" "$HOME/.config/$CFGDIR" && reassure "link new $CFGDIR config"
 done
 popd > /dev/null
+
+# install homebrew ------------------------------------------------------------
+declare "Installing the Xcode command line tools requies some GUI interaction."
+declare "Click 'Install' on the forthcoming alert box."
+declare "If you see '...already installed' then you're fine."
+xcode-select --install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+reassure "install homebrew"
 
 # run all the scripts in setup/scripts ----------------------------------------
 pushd ~/.config/danielbaird-osx/setup/scripts > /dev/null
 for SCRIPT in *.sh; do
     if [ -f $SCRIPT -a -x $SCRIPT ]; then
-        echo -n ${LBLUE}
         "$HOME/.config/danielbaird-osx/setup/scripts/$SCRIPT"
-        echo "${RESET}"
-        echo "$CHECK ${SCRIPT//-/ }" | sed 's/\.sh$//'
+        echo
+        reassure "${SCRIPT//-/ }" | sed 's/\.sh$//'
     fi
 done
 popd > /dev/null
