c5c5538c41a20c29fbef35959c088afa28c9aca9
Daniel Baird <daniel@danielbaird.com>
2017-03-20 16:00:31 +1000

    moved profile content to external file

---
+++ bin/bash-profile-content.sh
@@ -0,0 +1,60 @@
+# set -euo pipefail
+# IFS=$'\n\t'
+
+# grab the .bashrc if there is one
+if [ -f ~/.bashrc ]; then . ~/.bashrc; fi
+
+# bash completions via homebrew package "bash-completion"
+if [ -f $(brew --prefix)/etc/bash_completion ]; then
+    $(brew --prefix)/etc/bash_completion
+fi
+
+# git bash completion and prompt info
+source /usr/local/etc/bash_completion.d/git-completion.bash
+source /usr/local/etc/bash_completion.d/git-prompt.sh
+GIT_PS1_SHOWDIRTYSTATE=true
+
+# pull in colours
+source ~/.config/danielbaird-osx/bin/bash-color-vars.sh
+
+# set up the prompt
+export PS1="\n╭  ${RED}♥ ${GREY}\@ ${CYAN}\u${GREY} @ ${RED}\h ${GREY}in ${GREEN}\w ${RESTORE}\$(__git_ps1)\n╰  $ "
+
+# add my path then homebrew's path to the start of path
+export PATH="$HOME/.config/danielbaird-osx/bin:/usr/local/bin:$PATH"
+
+# terminal colouring
+export CLICOLOR=1
+export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
+
+#   cdf:  cd's to whatever the frontmost Finder window is showing
+cdf () {
+    currFolderPath=$( /usr/bin/osascript <<EOT
+        tell application "Finder"
+            try
+        set currFolder to (folder of the front window as alias)
+            on error
+        set currFolder to (path to desktop folder as alias)
+            end try
+            POSIX path of currFolder
+        end tell
+EOT
+    )
+    echo "cd to \"$currFolderPath\""
+    cd "$currFolderPath"
+}
+
+# switch on directory-based environment management
+eval "$(direnv hook bash)"
+echo "direnv is enabled; run  direnv allow <dir>  to enable for <dir>."
+
+alias cd..="cd .."
+alias ll="ls -halpF"
+alias grepr="grep -r"
+
+alias openports="lsof -i | grep LISTEN"
+alias clean-ds="find . -type f -name '*.DS_Store' -ls -delete"
+alias wget-mirror='wget --no-parent --no-check-certificate --html-extension --convert-links --restrict-file-names=windows --recursive --level=inf --page-requisites -e robots=off --wait=0 --quota=inf'
+
+# added by Anaconda3 4.3.1 installer
+export PATH="/Users/pvrdwb/anaconda3/bin:$PATH"
--- bin/bash-profile.sh
+++ bin/bash-profile.sh
@@ -1,61 +1,11 @@
-# set -euo pipefail
-# IFS=$'\n\t'
 
-# grab the .bashrc if there is one
-if [ -f ~/.bashrc ]; then . ~/.bashrc; fi
+#
+# all the version-controlled stuff in kept in bash-profile-content.sh,
+# so that lines added by installer programs can be easily identified.
+#
+source ~/.config/danielbaird-osx/bin/bash-profile-content.sh
 
-### apparently bash completions is broken in some way..
-# bash completions via homebrew package "bash-completion"
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-    $(brew --prefix)/etc/bash_completion
-fi
+#
+# --- anything below here was added by an installer or something ---
+#
 
-# git bash completion and prompt info
-source /usr/local/etc/bash_completion.d/git-completion.bash
-source /usr/local/etc/bash_completion.d/git-prompt.sh
-GIT_PS1_SHOWDIRTYSTATE=true
-
-# pull in colours
-source ~/.config/danielbaird-osx/bin/bash-color-vars.sh
-
-# set up the prompt
-export PS1="\n╭  ${RED}♥ ${GREY}\@ ${CYAN}\u${GREY} @ ${RED}\h ${GREY}in ${GREEN}\w ${RESTORE}\$(__git_ps1)\n╰  $ "
-# export PS1="\n${RED}♥ ${GREY}\@  ${CYAN}\u  ${GREY}@  ${RED}\h  ${GREY}in  ${GREEN}\w ${RESTORE}\n$ "
-# export PS1="\n\[${RED}\] <3  \[${GREY}\]\@  \[${CYAN}\]\u  \[${GREY}\]@  \[${RED}\]\h  \[${GREY}\]in  \[${GREEN}\]\w \[${RESTORE}\]\n ️$ "
-# export PS1="\n<3  \@  \u  @  \h  in  \w \n$ "
-
-# add my path then homebrew's path to the start of path
-export PATH="$HOME/.config/danielbaird-osx/bin:/usr/local/bin:$PATH"
-
-# terminal colouring
-export CLICOLOR=1
-export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
-
-#   cdf:  cd's to whatever the frontmost Finder window is showing
-cdf () {
-    currFolderPath=$( /usr/bin/osascript <<EOT
-        tell application "Finder"
-            try
-        set currFolder to (folder of the front window as alias)
-            on error
-        set currFolder to (path to desktop folder as alias)
-            end try
-            POSIX path of currFolder
-        end tell
-EOT
-    )
-    echo "cd to \"$currFolderPath\""
-    cd "$currFolderPath"
-}
-
-# switch on directory-based environment management
-eval "$(direnv hook bash)"
-echo "direnv is enabled; run  direnv allow <dir>  to enable for <dir>."
-
-alias cd..="cd .."
-alias ll="ls -halpF"
-alias grepr="grep -r"
-
-alias openports="lsof -i | grep LISTEN"
-alias clean-ds="find . -type f -name '*.DS_Store' -ls -delete"
-alias wget-mirror='wget --no-parent --no-check-certificate --html-extension --convert-links --restrict-file-names=windows --recursive --level=inf --page-requisites -e robots=off --wait=0 --quota=inf'
